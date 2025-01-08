ed806218ec0e9cf9612a4e0e48b3bbc4b1728d9a
nyeinphyoaung <nyeinphyoaung.edu@gmail.com>
2025-01-08 20:35:54 +0630

    complete draft UI for cards

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
     import Navbar from './components/Navbar.vue';
     import Hero from './components/Hero.vue';
+    import HomeCard from './components/HomeCard.vue';
 </script>
 
 <template>
     <Navbar/>
     <Hero title="Become a Vue Dev" subtitle="Find the Vue job that fits your skills and needs"/>
+    <HomeCard/>
 </template>
